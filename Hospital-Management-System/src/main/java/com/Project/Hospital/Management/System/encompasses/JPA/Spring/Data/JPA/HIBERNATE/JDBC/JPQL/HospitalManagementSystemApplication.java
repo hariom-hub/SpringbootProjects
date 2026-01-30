78c69b4b2b48dec843c00fce9b354b8cf4c08642
Hariom Singh Thakur <hariomthakur638@gmail.com>
2026-01-30 21:49:59 +0530

    project init

---
+++ Hospital-Management-System/src/main/java/com/Project/Hospital/Management/System/encompasses/JPA/Spring/Data/JPA/HIBERNATE/JDBC/JPQL/HospitalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Project.Hospital.Management.System.encompasses.JPA.Spring.Data.JPA.HIBERNATE.JDBC.JPQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Hospital-Management-System/src/test/java/com/Project/Hospital/Management/System/encompasses/JPA/Spring/Data/JPA/HIBERNATE/JDBC/JPQL/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.Hospital.Management.System.encompasses.JPA.Spring.Data.JPA.HIBERNATE.JDBC.JPQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
