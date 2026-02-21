68c465e14a178c42fee5be950847228c5029eeb8
harrybro <hariomthakur638@gmail.com>
2026-02-21 10:18:24 +0530

    @RequiredArgsConstructor added

--- Project-BloodBuddy/src/main/java/com/project/BloodBuddy/controller/patientRoutes.java
+++ Project-BloodBuddy/src/main/java/com/project/BloodBuddy/controller/patientRoutes.java
@@ -3,15 +3,17 @@ package com.project.BloodBuddy.controller;
 import java.util.*;
 import com.project.BloodBuddy.entitiy.patient;
 import com.project.BloodBuddy.service.PatientService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
+@RequiredArgsConstructor
 public class patientRoutes {
 
-    private PatientService patientService;
+    private final PatientService patientService;
     @GetMapping("/")
     public String GetDemo(){
         return "working correctly.";
